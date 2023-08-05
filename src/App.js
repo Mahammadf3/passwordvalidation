de562eb5b89863080fea41a048734fb188cdaf8c
Mahammadf3 <shaikmahammad622@gmail.com>
2023-08-05 09:48:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {useState} from "react"
+import "./App.css"
+const App=()=>{
+  const [name,setState]=useState("")
+const onPassword=event=>{
+setState(event.target.value)
+}
+const validation=()=>{
+  if(name.length<=8){
+    return<p className="hh"> password must be in 8 charectors</p>
+  }
+}
+  return(
+  <div className="container">
+    <div className="card">
+      <h1 className="h">
+        check your password
+      </h1>
+<p className="h">check how strong and check your password</p>
+<input type="password" onChange={onPassword}/>
+{validation()}
     </div>
-  );
+  </div>
+  )
 }
 
 export default App;
